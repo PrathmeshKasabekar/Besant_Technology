9d3f93f0e5942b4950d469fa4ccefed37f691b42
Prathmesh Kasabekar <kasabekarprathmesh@gmail.com>
2026-02-12 09:20:21 +0530

    push

---
+++ javascript/learn_operator.js
@@ -0,0 +1,48 @@
+/* 
+    Operator
+        1)Arithmatic Operator
+            a)Add(+)
+            b)sub(-)
+            c)mul(*)
+            d)div(/)
+            e)mod(%)
+            f)postincrement(a++)
+            g)preincriment(--a)
+            h)power(a**2)
+        2)Assignment Oprtor
+            i)i=i+10.  or i+=10;
+            ii)i=i*10.  or i*=10;
+            iii)i=i/10.  or i/=10;
+            iv)i=i%10.   or i%=2;
+            v)i=i**3.    or i**=2;
+        3)Conditional Operator
+
+        4)Relational Operator
+        5)ternary Oprator 
+*/
+
+// console.log(5 + 3);
+// console.log(5 - 3);
+// console.log(5 * 3);
+// console.log(5 / 3);
+// console.log(5 % 3);
+// console.log(5 ** 3);
+// let i = 10;
+// console.log(i++);
+// console.log(i++);
+
+// this is used for adding the number also can  be used as  i= i + 10;
+// let j = 10;
+// j += 10;
+// console.log(j);
+
+// datatype will not be taken under consideration in == so its giving the output as true
+console.log(5 == "5");
+// datatype will come undere consideration in === so its giving false output
+console.log(5 === "5");
+
+console.log(5 != 4);
+console.log(5 !== 5);
+
+console.log(5 > 5);
+console.log(5 >= 5);
