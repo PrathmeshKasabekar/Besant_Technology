e898fb22dbe7292c2bb0fbb7fcfbab85e9566bb7
Prathmesh Kasabekar <kasabekarprathmesh@gmail.com>
2026-02-23 09:06:22 +0530

    update

---
+++ javascript/arrow.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => {
+  return a + b;
+};
+let value = sum(5, 10);
+console.log(" i have the output", value);
+
+const sum2 = (a, b) => a + b;
+let value2 = sum(5, 10);
+console.log(" i have the output", value2);
---
+++ javascript/terinaryoperators.js
@@ -0,0 +1,3 @@
+let isPremium = true;
+let value = isPremium ? "Premium User" : "Normal User";
+console.log(value);
